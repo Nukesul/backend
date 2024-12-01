efb1bdcb08be89e6b3384f88e622d713605fa182
It developer 😎 <159410366+Nukesul@users.noreply.github.com>
2024-12-01 22:29:51 +0600

    21

--- index.js
+++ index.js
@@ -699,28 +699,42 @@ app.post('/api/login', (req, res) => {
         }
     });
 });
-
-
-
-// Маршрут для получения всех пользователей
-app.get('/api/user', authenticateToken, (req, res) => {
-    const userId = req.user.user_id; // ID пользователя из токена
-    const query = 'SELECT * FROM userskg WHERE user_id = ?';
-
-    db.query(query, [userId], (err, results) => {
-        if (err) {
-            console.error('Ошибка базы данных:', err);
-            return res.status(500).json({ message: 'Ошибка сервера' });
-        }
-
-        if (results.length === 0) {
-            return res.status(404).json({ message: 'Пользователь не найден' });
-        }
-
-        res.json(results[0]); // Отправка данных пользователя
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+  
+    if (!token) {
+      return res.status(403).json({ message: 'Токен не найден. Доступ запрещен.' });
+    }
+  
+    jwt.verify(token, 'your_secret_key', (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: 'Недействительный токен.' });
+      }
+      req.user = user; // Данные пользователя из токена
+      next();
     });
-});
-
+  };
+  
+  // Получение данных пользователя
+  app.get('/api/user', authenticateToken, (req, res) => {
+    const userId = req.user.user_id;
+  
+    const query = 'SELECT * FROM userskg WHERE user_id = ?';
+    db.query(query, [userId], (err, results) => {
+      if (err) {
+        console.error('Ошибка базы данных:', err);
+        return res.status(500).json({ message: 'Ошибка сервера' });
+      }
+  
+      if (results.length === 0) {
+        return res.status(404).json({ message: 'Пользователь не найден' });
+      }
+  
+      res.json(results[0]); // Отправка данных пользователя
+    });
+  });
+  
 
 app.delete('/api/users/:user_id', (req, res) => {
     const userId = parseInt(req.params.user_id); // Корректный парсинг user_id
