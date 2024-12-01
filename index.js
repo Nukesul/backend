b99d450a17581501568415793f562ee7a40efdb5
It developer 😎 <159410366+Nukesul@users.noreply.github.com>
2024-12-01 19:28:53 +0600

    112

--- index.js
+++ index.js
@@ -248,10 +248,7 @@ app.get('/api/send-order', async (req, res) => {
         let discount = parseFloat(req.query.discount) || 0; // Получаем скидку как число
 
         // Ограничиваем максимальную скидку
-        if (discount > 50) {
-            discount = 50; // Максимальная скидка 50%
-        }
-
+       
         const promoCodeUsed = discount > 0; // Проверяем, был ли использован промокод
 
         // Вычисляем итоговую стоимость товаров без скидки
